6dfc7d42b6673ff59fc5f5baf4cded2efc7ffa4d
raymondaksu <raymondaksu@users.noreply.github.com>
2020-10-28 01:43:42 +0300

    js-cc-06 updated v1.1

--- js-cc-06/script.js
+++ js-cc-06/script.js
@@ -10,6 +10,9 @@ function convert_to_roman(x) {
   // const myNumber = input.value;
 
     var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+    if (x == NaN || x < 1) {
+      alert('Please insert a valid entry!')
+    } else {
     for ( i in lookup ) {
       while ( x >= lookup[i] ) {
         roman += i;
@@ -19,6 +22,7 @@ function convert_to_roman(x) {
 
     const myNumber = input.value;
     result.innerHTML = `${myNumber} = ${roman}`;
+  }
 }
 
 toConvertButton.addEventListener("click", () => {
